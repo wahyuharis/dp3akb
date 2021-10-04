5711b63074ae9443767a52a20b4461278ec1f831
rosidf26 <frosid26@gmail.com>
2021-10-04 08:03:46 +0700

    add kodingan post data ke db menggunakan api lapor

--- application/controllers/api/Lapor.php
+++ application/controllers/api/Lapor.php
@@ -20,6 +20,13 @@ class Lapor extends RestController
 		$status = true;
 		$response = array();
 
+		$id = $this->get('id_korban');
+		if ($id == '') {
+			$data = $this->db->get('korban')->result();
+		} else {
+			$this->db->where('id_korban', $id);
+			$data = $this->db->get('korban')->result();
+		}
 
 		$response = array(
 			'data' => $data,
@@ -30,20 +37,33 @@ class Lapor extends RestController
 		$this->response($response, 200);
 	}
 
-    function save_post()
+	function save_post()
 	{
-		$data = array();
+		// $data = array();
 		$message = 'succes';
 		$status = true;
 		$response = array();
 
-        $post=$this->input->post();
+		//$post = $this->input->post();
+		//$data = $post;
 
-        $data=$post;
-
-
+		$data = array(
+			'id_korban'         => $this->post('id_korban'),
+			'id_pelapor'        => $this->post('id_pelapor'),
+			'id_jenis_aduan'    => $this->post('id_jenis_aduan'),
+			'jenis_korban'      => $this->post('jenis_korban'),
+			'nama_korban'       => $this->post('nama_korban'),
+			'jkel_korban'       => $this->post('jkel_korban'),
+			'umur_korban'       => $this->post('umur_korban'),
+			'nik_korban'        => $this->post('nik_korban'),
+			'nohp_korban'       => $this->post('nohp_korban'),
+			'alamat_korban'     => $this->post('alamat_korban'),
+			'aduan_lain'        => $this->post('aduan_lain'),
+			'status_laporan'    => $this->post('status_laporan'),
+		);
+		$insert = $this->db->insert('korban', $data);
 		$response = array(
-			'data' => $data,
+			'data' => $insert,
 			'message' => $message,
 			'status' => $status
 		);
