1fc64a4505c7ef282e4ffc2d9f5a7aa24dca84ae
harits wahyu <harits.wahyu.hid@gmail.com>
2021-10-06 08:16:31 +0700

    db baru

--- dp3akb.sql
+++ dp3akb.sql
@@ -13,6 +13,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Dumping structure for table dp3akb.jenis_pengaduan
+DROP TABLE IF EXISTS `jenis_pengaduan`;
 CREATE TABLE IF NOT EXISTS `jenis_pengaduan` (
   `id_jenis_aduan` int(11) NOT NULL AUTO_INCREMENT,
   `keterangan` varchar(255) NOT NULL,
@@ -31,6 +32,7 @@ INSERT INTO `jenis_pengaduan` (`id_jenis_aduan`, `keterangan`) VALUES
 /*!40000 ALTER TABLE `jenis_pengaduan` ENABLE KEYS */;
 
 -- Dumping structure for table dp3akb.korban
+DROP TABLE IF EXISTS `korban`;
 CREATE TABLE IF NOT EXISTS `korban` (
   `id_korban` int(11) NOT NULL AUTO_INCREMENT,
   `id_pelapor` int(11) DEFAULT NULL,
@@ -52,24 +54,26 @@ CREATE TABLE IF NOT EXISTS `korban` (
   KEY `id_jenis_aduan` (`id_jenis_aduan`),
   CONSTRAINT `korban_ibfk_1` FOREIGN KEY (`id_pelapor`) REFERENCES `pelapor` (`id_pelapor`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `korban_ibfk_2` FOREIGN KEY (`id_jenis_aduan`) REFERENCES `jenis_pengaduan` (`id_jenis_aduan`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table dp3akb.korban: ~6 rows (approximately)
+-- Dumping data for table dp3akb.korban: ~11 rows (approximately)
 /*!40000 ALTER TABLE `korban` DISABLE KEYS */;
 INSERT INTO `korban` (`id_korban`, `id_pelapor`, `id_jenis_aduan`, `jenis_korban`, `nama_korban`, `jkel_korban`, `umur_korban`, `nik_korban`, `nohp_korban`, `alamat_korban`, `aduan_lain`, `status_laporan`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, 1, 1, 'Anak', 'Ipsum Lorem 1', 'L', 12, '3509127654335791', '087612843999', 'Jl. Jalan', NULL, 1, '2021-09-20 03:18:55', '2021-10-01 03:52:44', NULL),
-	(2, 1, 3, 'Perempuan', 'Ipsum Lorem', 'P', 26, '3509127654339990', '087612843999', 'Jl. Indah', NULL, 1, '2021-09-29 20:39:13', '2021-10-01 03:53:19', NULL),
-	(3, 1, 2, 'Anak', 'Ipsum Lorem 3', 'P', 17, '3509127654335795', '087612843955', 'Jl. Ruang', '-', 2, '2021-10-04 00:58:00', '2021-10-04 00:58:00', NULL),
-	(4, 3, 1, 'Perempuan', 'haris', 'L', 12, NULL, '234234234234', 'sdfsdfsdf', '', NULL, '2021-10-05 04:00:47', '2021-10-05 04:00:47', NULL),
-	(5, 4, 1, 'Anak', 'haris', 'L', 12, NULL, '234234234234', 'sdfsdfsdf', '', NULL, '2021-10-05 04:05:59', '2021-10-05 04:05:59', NULL),
-	(6, 5, 1, 'Perempuan', 'sumiati', 'P', 34, NULL, '123123123123', 'jember', '', NULL, '2021-10-05 04:06:55', '2021-10-05 04:06:55', NULL),
-	(8, 8, NULL, 'Perempuan', 'sdfsdfsdfsd', 'P', 23, NULL, '345345345345', 'sdfsdfsdf', 'sdfsdfsdfsdfs', NULL, '2021-10-05 04:21:04', '2021-10-05 04:21:04', NULL),
-	(9, 9, 5, 'Perempuan', 'sdfsdfsdfsd', 'P', 23, NULL, '345345345345', 'sdfsdfsdf', 'sdfsdfsdfsdfs', NULL, '2021-10-05 04:21:33', '2021-10-05 04:21:33', NULL),
-	(10, 10, 2, 'Perempuan', 'fgdfgdfg', 'P', 23, NULL, '234234234234', 'sdfsdfsdf', '', NULL, '2021-10-05 04:22:59', '2021-10-05 04:22:59', NULL),
-	(11, 11, 1, 'Anak', 'Hhhhh', 'L', 12, NULL, '121212121212', 'Jember', '', NULL, '2021-10-05 04:28:31', '2021-10-05 04:28:31', NULL);
+	(1, 1, 1, 'Anak', 'Ipsum Lorem 1', 'L', 12, '3509127654335791', '087612843999', 'Jl. Jalan', NULL, 1, '2021-09-20 10:18:55', '2021-10-01 10:52:44', NULL),
+	(2, 1, 3, 'Perempuan', 'Ipsum Lorem', 'P', 26, '3509127654339990', '087612843999', 'Jl. Indah', NULL, 1, '2021-09-30 03:39:13', '2021-10-01 10:53:19', NULL),
+	(3, 1, 2, 'Anak', 'Ipsum Lorem 3', 'P', 17, '3509127654335795', '087612843955', 'Jl. Ruang', '-', 2, '2021-10-04 07:58:00', '2021-10-04 07:58:00', NULL),
+	(4, 3, 1, 'Perempuan', 'haris', 'L', 12, NULL, '234234234234', 'sdfsdfsdf', '', NULL, '2021-10-05 11:00:47', '2021-10-05 11:00:47', NULL),
+	(5, 4, 1, 'Anak', 'haris', 'L', 12, NULL, '234234234234', 'sdfsdfsdf', '', NULL, '2021-10-05 11:05:59', '2021-10-05 11:05:59', NULL),
+	(6, 5, 1, 'Perempuan', 'sumiati', 'P', 34, NULL, '123123123123', 'jember', '', NULL, '2021-10-05 11:06:55', '2021-10-05 11:06:55', NULL),
+	(8, 8, NULL, 'Perempuan', 'sdfsdfsdfsd', 'P', 23, NULL, '345345345345', 'sdfsdfsdf', 'sdfsdfsdfsdfs', NULL, '2021-10-05 11:21:04', '2021-10-05 11:21:04', NULL),
+	(9, 9, 5, 'Perempuan', 'sdfsdfsdfsd', 'P', 23, NULL, '345345345345', 'sdfsdfsdf', 'sdfsdfsdfsdfs', NULL, '2021-10-05 11:21:33', '2021-10-05 11:21:33', NULL),
+	(10, 10, 2, 'Perempuan', 'fgdfgdfg', 'P', 23, NULL, '234234234234', 'sdfsdfsdf', '', NULL, '2021-10-05 11:22:59', '2021-10-05 11:22:59', NULL),
+	(11, 11, 1, 'Anak', 'Hhhhh', 'L', 12, NULL, '121212121212', 'Jember', '', NULL, '2021-10-05 11:28:31', '2021-10-05 11:28:31', NULL),
+	(12, 12, NULL, 'Anak', 'Lorem Ipsum', 'P', 15, NULL, '085655322911', 'Jl. Semeru', 'Testing', NULL, '2021-10-05 14:51:19', '2021-10-05 14:51:19', NULL);
 /*!40000 ALTER TABLE `korban` ENABLE KEYS */;
 
 -- Dumping structure for table dp3akb.pelapor
+DROP TABLE IF EXISTS `pelapor`;
 CREATE TABLE IF NOT EXISTS `pelapor` (
   `id_pelapor` int(11) NOT NULL AUTO_INCREMENT,
   `nama_pelapor` varchar(255) NOT NULL,
@@ -82,25 +86,27 @@ CREATE TABLE IF NOT EXISTS `pelapor` (
   `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id_pelapor`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table dp3akb.pelapor: ~7 rows (approximately)
+-- Dumping data for table dp3akb.pelapor: ~11 rows (approximately)
 /*!40000 ALTER TABLE `pelapor` DISABLE KEYS */;
 INSERT INTO `pelapor` (`id_pelapor`, `nama_pelapor`, `jkel_pelapor`, `umur_pelapor`, `nik_pelapor`, `nohp_pelapor`, `alamat_pelapor`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, 'Lorem Ipsum 1', 'L', 20, '3509190405980001', '085123465888', 'Jl. Jalan', '2021-09-20 03:16:13', '2021-10-04 01:01:37', NULL),
-	(2, 'Lorem Ipsum 2', 'P', 25, '3509810605870002', '089777444222', 'Jl. Pegangsaan Timur No. 56, Jakarta', '2021-09-20 03:16:13', '2021-10-04 01:01:40', NULL),
-	(3, 'haris', 'L', 12, NULL, '3542334234234', 'asdfasdsad', '2021-10-05 04:00:47', '2021-10-05 04:00:47', NULL),
-	(4, 'haris', 'L', 12, NULL, '3542334234234', 'asdfasdsad', '2021-10-05 04:05:59', '2021-10-05 04:05:59', NULL),
-	(5, 'haris', 'L', 12, NULL, '123123123123123', 'jember', '2021-10-05 04:06:55', '2021-10-05 04:06:55', NULL),
-	(6, 'sdgsdgsdf', 'L', 34, NULL, '346345345345', 'dfgdfgdfg', '2021-10-05 04:17:31', '2021-10-05 04:17:31', NULL),
-	(7, 'sdgsdgsdf', 'L', 34, NULL, '346345345345', 'dfgdfgdfg', '2021-10-05 04:19:54', '2021-10-05 04:19:54', NULL),
-	(8, 'sdgsdgsdf', 'L', 34, NULL, '346345345345', 'dfgdfgdfg', '2021-10-05 04:21:04', '2021-10-05 04:21:04', NULL),
-	(9, 'sdgsdgsdf', 'L', 34, NULL, '346345345345', 'dfgdfgdfg', '2021-10-05 04:21:33', '2021-10-05 04:21:33', NULL),
-	(10, 'fdsgdfg', 'L', 23, NULL, '234234234234', 'dfgdfgdfg', '2021-10-05 04:22:59', '2021-10-05 04:22:59', NULL),
-	(11, 'Haris', 'L', 29, NULL, '085330067932', 'Jember', '2021-10-05 04:28:31', '2021-10-05 04:28:31', NULL);
+	(1, 'Lorem Ipsum 1', 'L', 20, '3509190405980001', '085123465888', 'Jl. Jalan', '2021-09-20 10:16:13', '2021-10-04 08:01:37', NULL),
+	(2, 'Lorem Ipsum 2', 'P', 25, '3509810605870002', '089777444222', 'Jl. Pegangsaan Timur No. 56, Jakarta', '2021-09-20 10:16:13', '2021-10-04 08:01:40', NULL),
+	(3, 'haris', 'L', 12, NULL, '3542334234234', 'asdfasdsad', '2021-10-05 11:00:47', '2021-10-05 11:00:47', NULL),
+	(4, 'haris', 'L', 12, NULL, '3542334234234', 'asdfasdsad', '2021-10-05 11:05:59', '2021-10-05 11:05:59', NULL),
+	(5, 'haris', 'L', 12, NULL, '123123123123123', 'jember', '2021-10-05 11:06:55', '2021-10-05 11:06:55', NULL),
+	(6, 'sdgsdgsdf', 'L', 34, NULL, '346345345345', 'dfgdfgdfg', '2021-10-05 11:17:31', '2021-10-05 11:17:31', NULL),
+	(7, 'sdgsdgsdf', 'L', 34, NULL, '346345345345', 'dfgdfgdfg', '2021-10-05 11:19:54', '2021-10-05 11:19:54', NULL),
+	(8, 'sdgsdgsdf', 'L', 34, NULL, '346345345345', 'dfgdfgdfg', '2021-10-05 11:21:04', '2021-10-05 11:21:04', NULL),
+	(9, 'sdgsdgsdf', 'L', 34, NULL, '346345345345', 'dfgdfgdfg', '2021-10-05 11:21:33', '2021-10-05 11:21:33', NULL),
+	(10, 'fdsgdfg', 'L', 23, NULL, '234234234234', 'dfgdfgdfg', '2021-10-05 11:22:59', '2021-10-05 11:22:59', NULL),
+	(11, 'Haris', 'L', 29, NULL, '085330067932', 'Jember', '2021-10-05 11:28:31', '2021-10-05 11:28:31', NULL),
+	(12, 'Fathor Rosid', 'L', 24, NULL, '081233846841', 'Jl. Raung', '2021-10-05 14:51:18', '2021-10-05 14:51:18', NULL);
 /*!40000 ALTER TABLE `pelapor` ENABLE KEYS */;
 
 -- Dumping structure for table dp3akb.users
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `fullname` varchar(255) NOT NULL,
@@ -117,9 +123,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table dp3akb.users: ~3 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id_user`, `fullname`, `email`, `password`, `jabatan`, `level`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, 'Lorem Ipsum', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 'Ka. Bidang Keluarga Berencana', 'Superadmin', '2021-09-20 03:13:22', '2021-09-24 07:06:37', NULL),
-	(2, 'Lorem Ipsum 2', 'admin2@admin2.com', 'c84258e9c39059a89ab77d846ddab909', 'Ka. Bidang Keluarga Sejahtera', 'Admin', '2021-09-20 03:13:22', '2021-09-27 03:46:43', NULL),
-	(5, 'testes', 'testes@gmail.com', '28b662d883b6d76fd96e4ddc5e9ba780', 'hgfhgfh', 'Admin', '2021-09-27 03:47:31', '2021-09-27 03:47:31', NULL);
+	(1, 'Lorem Ipsum', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 'Ka. Bidang Keluarga Berencana', 'Superadmin', '2021-09-20 10:13:22', '2021-09-24 14:06:37', NULL),
+	(2, 'Lorem Ipsum 2', 'admin2@admin2.com', 'c84258e9c39059a89ab77d846ddab909', 'Ka. Bidang Keluarga Sejahtera', 'Admin', '2021-09-20 10:13:22', '2021-09-27 10:46:43', NULL),
+	(5, 'testes', 'testes@gmail.com', '28b662d883b6d76fd96e4ddc5e9ba780', 'hgfhgfh', 'Admin', '2021-09-27 10:47:31', '2021-09-27 10:47:31', NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
